5f32429fcb961d783e9392adc44f097525cd3758
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-04-10 17:06:06 +0000

    Compile Sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.imad5112a1_2025"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.imad5112a1_2025"
