c7ba7ca493e3e5aa4fb299f4d8a9e6d445642e3c
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-04-10 17:12:44 +0000

    Some boiler plate code

--- app/src/main/java/com/example/imad5112a1_2025/MainActivity.kt
+++ app/src/main/java/com/example/imad5112a1_2025/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.imad5112a1_2025
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val editTextText = findViewById<EditText>(R.id.editTextText)
+        val button = findViewById<Button>(R.id.button)
+
+
+
+
+        button?.setOnClickListener {
+            var greeting: String = ""
+
+
+            greeting = "Welcome, ${editTextText.text}"
+            textView.text = greeting
+
+        }
     }
 }
